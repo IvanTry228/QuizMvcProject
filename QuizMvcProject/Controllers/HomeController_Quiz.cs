1c584d3dedf171fcc0e70d3516beba42be776e90
IvanTry228 <65556657+IvanTry228@users.noreply.github.com>
2021-12-21 16:56:06 +0300

    Update HomeController_Quiz.cs

--- QuizMvcProject/Controllers/HomeController_Quiz.cs
+++ QuizMvcProject/Controllers/HomeController_Quiz.cs
@@ -43,23 +43,17 @@ namespace QuizMvcProject.Controllers
         }
 
         [HttpPost]
-        public IActionResult QuestionNotExist(string login, string password)
+        public IActionResult QuestionNotExist(string id)
         {
-            string authData = $"Login: {login}   Password: {password}";
-            Console.WriteLine("QuestionNotExist authData = " + authData);
-            int testInt = Int32.Parse(password);
-            Console.WriteLine("QuestionNotExist testInt = " + testInt);
-            return RedirectToAction("Question", new { id = testInt });
+            int parsedId = int.Parse(id);
+            Console.WriteLine("QuestionNotExist parsedId = " + parsedId);
+            return RedirectToAction("Question", new { id = parsedId });
         }
-
-        //static QuizPageModel fastQuizPageModel = new QuizPageModel(true); //static for fast
 
         [HttpGet]
         public IActionResult GetRandomQuiz()
         {
-            //MessageModel currentessage = new MessageModel("Qestion with current id not exist = " + id);
-            Console.WriteLine("!!!!!!!!!!!___!!! GetRandomQuiz ");
-
+            Console.WriteLine("GetRandomQuiz ");
             int bufferHashCode = -1;
             QuizPageModel fastQuizPageModel = QuizModelsPoolManager.Instance.GetQuizPageModelFree(out bufferHashCode); // new QuizPageModel(true);
             int defaultId = 0;
@@ -70,15 +64,12 @@ namespace QuizMvcProject.Controllers
         [HttpGet]
         public IActionResult QuizPage(int id, int hashCode)
         {
-            Console.WriteLine("!!!!!!!!!!!___!!! QuizPage id = " + id + " hashCode = " + hashCode);
-            //MessageModel currentessage = new MessageModel("Qestion with current id not exist = " + id);
+            Console.WriteLine("QuizPage id = " + id + " hashCode = " + hashCode);
 
             QuizPageModel fastQuizPageModel = QuizModelsPoolManager.Instance.GetQuizPageModelFrFromHash(hashCode); // new QuizPageModel(true);
 
             if(fastQuizPageModel==null)
-            {
                 return RedirectToAction("QuizHashCodeNotExist", new { id = hashCode });
-            }
 
             fastQuizPageModel.GetQuizBase().SetCurrenPointertIndex(id);
 
@@ -89,10 +80,11 @@ namespace QuizMvcProject.Controllers
         public IActionResult CallAnswerForQuestion(int _indexQuestion, int _indexAnswer, int _hashCode)
         {
             Console.WriteLine("!_!!! CallAnswerForQuestion _indexQuestion = " + _indexQuestion + " _indexAnswer = " + _indexAnswer + " _hashCode =" + _hashCode);
-            //MessageModel currentessage = new MessageModel("Qestion with current id not exist = " + id);
-            //fastQuizPageModel.GetQuizBase().SetCurrenPointertIndex(id);
-            //QuizPageModel fastQuizPageModel.GetQuizBase().CallAnswerForQuestion(_indexQuestion, _indexAnswer);
             QuizPageModel fastQuizPageModel = QuizModelsPoolManager.Instance.GetQuizPageModelFrFromHash(_hashCode); // new QuizPageModel(true);
+
+            if(fastQuizPageModel==null)
+                return RedirectToAction("QuizHashCodeNotExist", new { id = _hashCode });
+
             fastQuizPageModel.GetQuizBase().CallAnswerForQuestion(_indexQuestion, _indexAnswer);
             fastQuizPageModel.GetQuizBase().CallNextIndex();
             return RedirectToAction("QuizPage", new { id = fastQuizPageModel.GetQuizBase().GetCurrenPointertIndex(), hashCode = _hashCode });
